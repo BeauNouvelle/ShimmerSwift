3a5d9a96201bdc60d3dca9e28168709d4399e83f
Beau Nouvelle <beau.r.nouvelle@icloud.com>
2018-09-07 11:07:38 +1000

    Project Setup

---
+++ FBShimmerSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FBShimmerSwift
+//
+//  Created by Beau Nouvelle on 7/9/18.
+//  Copyright © 2018 nouvelle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
